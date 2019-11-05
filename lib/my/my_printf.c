5381c867a818fb9d9aa79a74761249fe41dfb611
Julien Pause <julien.pause@epitech.eu>
2019-11-06 00:10:24 +0400

    Add print_token function in my_printf

--- lib/my/my_printf.c
+++ lib/my/my_printf.c
@@ -2,19 +2,15 @@
 ** EPITECH PROJECT, 2019
 ** my_printf
 ** File description:
-** my_printf main file
-*/
-
-/*
-** EPITECH PROJECT, 2019
-** my_printf
-** File description:
-** my_printf
+** Reproduce the printf function from the C library, respecting C99 standard
 */
 
 #include <stdarg.h>
+#include <stddef.h>
 #include "my.h"
+#include "format_id.h"
 
+int print_token(char const *format, int *i, va_list args);
 void print_type(char const my_char, va_list args);
 
 int my_printf(char const *format, ...)
@@ -22,32 +18,34 @@ int my_printf(char const *format, ...)
     va_list args;
 
     va_start(args, format);
-    for (int i = 0 ; format[i] ; i = i + 1) {
-        if (format[i] == '%') {
-            print_type(format[(i + 1)], args);
-            i = i + 1;
-        } else
-            my_putchar(format[i]);
-    }
+    for (int i = 0 ; format[i] ; i = i + 1)
+        if (print_token(format, &i, args) == MY_EXIT_FAILURE)
+            return (MY_EXIT_FAILURE);
     va_end(args);
     return (0);
 }
 
+int print_token(char const *format, int *i, va_list args)
+{
+    if (format[*i] == '%') {
+        if (my_is_format_id(format[(*i + 1)])) {
+            print_type(format[(*i + 1)], args);
+            *i = *i + 1;
+        } else {
+            my_puterr(UNKNOWN_ID_MSG);
+            return (MY_EXIT_FAILURE);
+        }
+    } else
+        my_putchar(format[*i]);
+    return (MY_EXIT_SUCCESS);
+}
+
 void print_type(char const my_char, va_list args)
 {
-    int my_int = 0;
-    //float my_float = 0.0;
-    //double my_double = 0.0;
+    char *str = NULL;
+    format_id_t id;
 
-    if (my_char == 'd') {
-        my_int = va_arg(args, int);
-        char *tmp = my_int_to_strnum(my_int);
-        my_putstr(tmp);
-    }
-    if (my_char == '%')
-        my_putchar('%');
-    /*if (my_char == 'f' || my_char == 'd') {
-        my_double = va_arg(args, double);
-        my_put_float(my_float);
-    }*/
+    id = my_get_format_id(my_char);
+    str = id.convertion(args);
+    my_putstr(str);
 }

9f03476312fad4a88c43800140deb8e91115737b
Julien Pause <julien.pause@epitech.eu>
2019-11-06 00:09:49 +0400

    Rename my_is_format_id.c to my_format_id.c

---
+++ lib/my/my_format_id.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2019
+** my_printf
+** File description:
+** Format structure
+*/
+
+#include <stdbool.h>
+#include "format_id.h"
+#include "my.h"
+
+int my_strlen(char const *str);
+
+bool my_is_format_id(char my_char)
+{
+    char const symbols[] = FORMATS_ID;
+
+    for (int i = 0 ; i < my_strlen(symbols) ; i = i + 1)
+        if (my_char == symbols[i])
+            return (true);
+    return (false);
+}
+
+format_id_t my_get_format_id(char my_char)
+{
+    format_id_t const formats[] = {
+        {'b', &my_decimal_to_binary},
+        {'c', &my_char_to_str},
+        {'d', &my_int_to_strnum}, {'i', &my_int_to_strnum},
+        {'s', &my_arg_to_str},
+        {'S', &my_np_to_str},
+        {'%', &my_percentage_to_str}
+    };
+    for (int i = 0 ; i < 7 ; i = i + 1)
+        if (my_char == formats[i].symbol)
+            return (formats[i]);
+    return (formats[5]);
+}

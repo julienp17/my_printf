790cb1cbb1d30164825de15f57d713d2d6e9ac14
Julien Pause <julien.pause@epitech.eu>
2019-11-06 00:11:34 +0400

    Add my_strcat to libmy

---
+++ lib/my/my_strcat.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** my_printf
+** File description:
+** Concatenantes two strings
+*/
+
+char *my_strcat(char *dest, char const *src)
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0')
+        i = i + 1;
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i = i + 1;
+        j = j + 1;
+    }
+    dest[i] = '\0';
+    return (dest);
+}

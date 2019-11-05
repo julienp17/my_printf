b9ec0bb75b9d88c6f8f028275b527f72e5f60f5d
Julien Pause <julien.pause@epitech.eu>
2019-11-06 00:07:02 +0400

    Add new functions prototypes in my.h

--- include/my.h
+++ include/my.h
@@ -9,17 +9,29 @@
     #define __DEF_MY__
 
     #include <stdarg.h>
+    #include <stdbool.h>
 
     #define MY_EXIT_SUCCESS     0
     #define MY_EXIT_FAILURE     84
 
     void my_putchar(char my_char);
     void my_putstr(char const *str);
+    void my_puterr(char const *str);
+
+    bool my_is_printable(char const my_char);
 
     int my_printf(const char *restrict format, ...);
     int my_strlen(char const *str);
     int my_put_nbr(int nb);
 
     char *my_revstr(char *str);
-    char *my_int_to_strnum(int nb);
+    char *my_char_to_octal(char my_char);
+    char *my_strcat(char *dest, char const *src);
+
+    char *my_decimal_to_binary(va_list args);
+    char *my_char_to_str(va_list args);
+    char *my_arg_to_str(va_list args);
+    char *my_percentage_to_str(va_list args);
+    char *my_np_to_str(va_list args);
+    char *my_int_to_strnum(va_list arg);
 #endif

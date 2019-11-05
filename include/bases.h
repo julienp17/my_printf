8b64a6a12e5e7f77f8b0117259974a3153e97540
Julien Pause <julien.pause@epitech.eu>
2019-11-05 21:39:36 +0400

    Add include/bases.h

---
+++ include/bases.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** my_printf
+** File description:
+** Header file for libmy.a
+*/
+
+#ifndef __DEF_BASES__
+    #define __DEF_BASES__
+
+    #define BINARY_MAX_DIGITS       32
+    #define OCTAL_MAX_LEN           4
+#endif

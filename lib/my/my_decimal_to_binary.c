a17d7a2c3ffa418327c7d791c74ae41e8ac0c570
Julien Pause <julien.pause@epitech.eu>
2019-11-06 16:26:12 +0400

    Put a backslash zero at the end of strnum in my_decimal_to_binary

--- lib/my/my_decimal_to_binary.c
+++ lib/my/my_decimal_to_binary.c
@@ -15,13 +15,15 @@ char *my_decimal_to_binary(va_list args)
     unsigned int decimal = va_arg(args, unsigned int);
     char *binary = malloc(sizeof(char) * (BINARY_MAX_LEN + 1));
     int digit = 0;
+    int i = 0;
 
     if (decimal == 0)
         return ("0");
-    for (int i = 0 ; decimal > 0 ; i = i + 1) {
+    for (i = 0 ; decimal > 0 ; i = i + 1) {
         digit = decimal % 2;
         binary[i] = digit + '0';
         decimal = decimal / 2;
     }
+    binary[i] = '\0';
     return (my_revstr(binary));
 }

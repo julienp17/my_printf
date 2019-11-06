f99daa12194d2be3181df65804e3ffe2cdff73a4
Julien Pause <julien.pause@epitech.eu>
2019-11-06 10:44:33 +0400

    Fix my_printf_no_args unit tests

--- tests/test_my_printf_no_args.c
+++ tests/test_my_printf_no_args.c
@@ -2,28 +2,26 @@
 ** EPITECH PROJECT, 2019
 ** my_printf
 ** File description:
-** Unit tests for my_printf
+** Unit tests for my_printf with no variadic arguments
 */
 
-void redirect_all_std(void)
-{
-    cr_redirect_stdout();
-    cr_redirect_stderr();
-}
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "my.h"
 
-Test(my_printf_no_args, hello_world, .init = redirect_all_std)
+Test(my_printf_no_args, hello_world, .init = cr_redirect_stdout)
 {
     my_printf("hello world");
     cr_assert_stdout_eq_str("hello world");
 }
 
-Test(my_printf_no_args, one_char_in_str, .init = redirect_all_std)
+Test(my_printf_no_args, one_char_in_str, .init = cr_redirect_stdout)
 {
     my_printf("h");
     cr_assert_stdout_eq_str("h");
 }
 
-Test(my_printf_no_args, one_newline_in_str, .init = redirect_all_std)
+Test(my_printf_no_args, one_newline_in_str, .init = cr_redirect_stdout)
 {
     my_printf("\n");
     cr_assert_stdout_eq_str("\n");

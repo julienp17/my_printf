7105ad77fb348d57cc15c81fed6c4d0d521eeda3
Julien Pause <julien.pause@epitech.eu>
2019-11-06 16:28:18 +0400

    Fix my_uint_to_strnum

--- lib/my/my_uint_to_strnum.c
+++ lib/my/my_uint_to_strnum.c
@@ -7,20 +7,21 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include "bases.h"
 #include "my.h"
-#include <stdio.h>
+
 char *my_uint_to_strnum(va_list args)
 {
-    unsigned int nb = 0;
-    nb = va_arg(args, unsigned int);
-    int len = my_intlen(nb);
-    char *strnum = malloc(sizeof(char) * (len + 1));
+    int i = 0;
+    unsigned int nb = va_arg(args, unsigned int);
+    char *strnum = malloc(sizeof(char) * (UINT_MAX_LEN + 1));
 
-    //printf("unsigned number = %d\n", nb);
-    for (int i = 0 ; nb != 0 ; i = i + 1) {
+    if (nb == 0)
+        return ("0");
+    for (; nb != 0 ; i = i + 1) {
         strnum[i] = nb % 10 + '0';
         nb = nb / 10;
     }
-    strnum[len] = '\0';
+    strnum[i] = '\0';
     return (my_revstr(strnum));
 }

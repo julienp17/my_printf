5aa908be8704a931428f86a7348e6e0c6a314172
Julien Pause <julien.pause@epitech.eu>
2019-11-06 10:23:19 +0400

    Change test_my_decimal_to_binary to check stdout output rather than char* return

--- tests/test_my_decimal_to_binary.c
+++ tests/test_my_decimal_to_binary.c
@@ -8,95 +8,86 @@
 #include <stddef.h>
 #include <limits.h>
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "my.h"
 
-char *my_decimal_to_binary(unsigned int decimal);
-
-Test(my_decimal_to_binary, zero)
+Test(my_decimal_to_binary, zero, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 0;
-    char *binary = NULL;
     char expected[] = "0";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, one)
+Test(my_decimal_to_binary, one, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 1;
-    char *binary = NULL;
     char expected[] = "1";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, two)
+Test(my_decimal_to_binary, two, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 2;
-    char *binary = NULL;
     char expected[] = "10";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, three)
+Test(my_decimal_to_binary, three, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 3;
-    char *binary = NULL;
     char expected[] = "11";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, ten)
+Test(my_decimal_to_binary, ten, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 10;
-    char *binary = NULL;
     char expected[] = "1010";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, one_hundred)
+Test(my_decimal_to_binary, one_hundred, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 100;
-    char *binary = NULL;
     char expected[] = "1100100";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, one_thousand)
+Test(my_decimal_to_binary, one_thousand, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 1000;
-    char *binary = NULL;
     char expected[] = "1111101000";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-Test(my_decimal_to_binary, ten_thousand)
+Test(my_decimal_to_binary, ten_thousand, .init = cr_redirect_stdout)
 {
     unsigned int decimal = 10000;
-    char *binary = NULL;
     char expected[] = "10011100010000";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
 }
 
-/*Test(my_decimal_to_binary, unsigned_int_max)
+Test(my_decimal_to_binary, uint_max, .init = cr_redirect_stdout)
 {
     unsigned int decimal = UINT_MAX;
-    char *binary = NULL;
     char expected[] = "11111111111111111111111111111111";
 
-    binary = my_decimal_to_binary(decimal);
-    cr_assert_str_eq(binary, expected);
-}*/
+    my_printf("%b", decimal);
+    cr_assert_stdout_eq_str(expected);
+}

bccd2d0be0cd9f0b94116ebfe55193a228bb116f
Julien Pause <julien.pause@epitech.eu>
2019-11-10 23:17:11 +0400

    Promote int to char in my_char_to_str.c

--- lib/my/my_char_to_str.c
+++ lib/my/my_char_to_str.c
@@ -12,7 +12,7 @@ char *my_char_to_str(va_list args)
 {
     char *str = malloc(sizeof(char) * 2);
 
-    str[0] = va_arg(args, int);
+    str[0] = (char)va_arg(args, int);
     str[1] = '\0';
     return (str);
 }

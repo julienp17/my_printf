d533720825fb81d01018b3578af75a64e7c62ac4
Julien Pause <julien.pause@epitech.eu>
2019-11-13 19:30:04 +0400

    Refactor my_printf completely

--- lib/my/my_printf.c
+++ lib/my/my_printf.c
@@ -5,43 +5,93 @@
 ** Reproduce the printf function in accordance to the C99 standard
 */
 
+#include <stdio.h>
+
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include "my.h"
-#include "format_id.h"
+#include "converters.h"
+#include "flags.h"
+#include "length_modifiers.h"
 
-int print_token(char const *format, int *i, va_list args);
-int print_type(char const my_char, va_list args);
+int my_print_tokens(char **format_string, va_list args);
+char *my_get_formatted_string(char *format, va_list args);
+char *my_get_format(char const *format_string);
+char *(*my_get_length_modifier(char *str, char const converter))(va_list args);
 
-int my_printf(char const *format, ...)
+int my_printf(char const *str, ...)
 {
     va_list args;
+    char *format_string = my_strdup(str);
+    int chars_written = 0;
 
-    va_start(args, format);
-    for (int i = 0 ; format[i] ; i = i + 1)
-        print_token(format, &i, args);
+    va_start(args, str);
+    while (format_string[0])
+        chars_written = chars_written + my_print_tokens(&format_string, args);
     va_end(args);
-    return (0);
+    return (chars_written);
 }
 
-int print_token(char const *format, int *i, va_list args)
+int my_print_tokens(char **format_string, va_list args)
 {
-    if (format[*i] == '%' && my_is_format_id(format[(*i + 1)])) {
-        print_type(format[(*i + 1)], args);
-        *i = *i + 1;
+    char *format = NULL;
+    char *formatted_string = NULL;
+    int chars_written = 0;
+
+    if ((*format_string)[0] == '%') {
+        format = my_get_format(*format_string);
+        (*format_string) = (*format_string) + my_strlen(format);
+        formatted_string = my_get_formatted_string(format, args);
+        chars_written = chars_written + my_strlen(formatted_string);
+        my_putstr(formatted_string);
     } else {
-        my_putchar(format[*i]);
+        my_putchar((*format_string)[0]);
+        (*format_string) = (*format_string) + 1;
+        chars_written = chars_written + 1;
     }
-    return (MY_EXIT_SUCCESS);
+    return (chars_written);
+}
+
+char *my_get_format(char const *format_string)
+{
+    char *format = malloc(sizeof(char) * (my_strlen(format_string) + 1));
+    int i = 0;
+
+    while (format_string[i] && !my_is_converter(format_string[i])) {
+        format[i] = format_string[i];
+        i = i + 1;
+    }
+    format[i] = '\0';
+    return (format);
+}
+
+char *my_get_formatted_string(char *format, va_list args)
+{
+    char *formatted_string = malloc(sizeof(char) * (my_strlen(format) + 6));
+    converter_t converter;
+    char *(*length_modifier)(va_list);
+
+    if (!my_is_converter(format[my_strlen(format)]))
+        return (format);
+    format = format + 1;
+    converter = my_get_converter_specifier(format[my_strlen(format) - 1]);
+    format[my_strlen(format) - 1] = '\0';
+    if (!format[0])
+        return (converter.convertion(args));
+    length_modifier = my_get_length_modifier(format, converter.symbol);
+    if (length_modifier)
+        return (length_modifier(args));
+    return (formatted_string);
 }
 
 int print_type(char const my_char, va_list args)
 {
     char *str = NULL;
-    format_id_t id;
+    struct converter_specifier converter;
 
-    id = my_get_format_id(my_char);
-    str = id.convertion(args);
+    converter = my_get_converter_specifier(my_char);
+    str = converter.convertion(args);
     my_putstr(str);
     return (my_strlen(str));
 }

1c12705b1b29fb901ad6a6c832bec5581a74d9f2
Julien Pause <julien.pause@epitech.eu>
2019-11-13 19:30:50 +0400

    Move converters unit tests in tests/converters and same thing for booleans

--- tests/booleans/test_my_is_converter.c
+++ tests/booleans/test_my_is_converter.c
@@ -2,90 +2,90 @@
 ** EPITECH PROJECT, 2019
 ** my_printf
 ** File description:
-** Unit tests for my_is_format_id
+** Unit tests for my_is_converter
 */
 
 #include <stdbool.h>
 #include <criterion/criterion.h>
 
-bool my_is_format_id(char my_char);
+bool my_is_converter(char my_char);
 
-Test(my_is_format_id, char_id)
+Test(my_is_converter, char_id)
 {
     char my_char = 'c';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, int_id_d)
+Test(my_is_converter, int_id_d)
 {
     char my_char = 'd';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, int_id_i)
+Test(my_is_converter, int_id_i)
 {
     char my_char = 'i';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, str_id)
+Test(my_is_converter, str_id)
 {
     char my_char = 's';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, non_printable_str_id)
+Test(my_is_converter, non_printable_str_id)
 {
     char my_char = 'S';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, percentage_id)
+Test(my_is_converter, percentage_id)
 {
     char my_char = '%';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, binary_id)
+Test(my_is_converter, binary_id)
 {
     char my_char = 'b';
     bool actual = 0;
     bool expected = true;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }
 
-Test(my_is_format_id, not_an_id)
+Test(my_is_converter, not_an_id)
 {
     char my_char = ';';
     bool actual = 0;
     bool expected = false;
 
-    actual = my_is_format_id(my_char);
+    actual = my_is_converter(my_char);
     cr_assert_eq(actual, expected);
 }

4f2998b198bfa981bdb4529e6cad2d54ea4066e6
Julien Pause <julien.pause@epitech.eu>
2019-11-15 11:04:31 +0400

    Refacor my_get_length_modifier to return a pointer to struct length_modifier_t instead of a function pointer

--- lib/my/my_get_length_modifier.c
+++ lib/my/my_get_length_modifier.c
@@ -7,31 +7,60 @@
 
 #include <stddef.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include "length_modifiers.h"
 #include "converters.h"
 #include "my.h"
 
-static char *(*get_ll_convertion(char symbol))(va_list args);
+static length_modifier_t *get_h_or_hh_length_modifier(char *rev_str,
+                                                    converter_t *converter);
+static length_modifier_t *get_l_or_ll_length_modifier(char *rev_str,
+                                                    converter_t *converter);
+static char *(*get_unsigned_convertion(char symbol))(va_list);
 
-char *(*my_get_length_modifier(char *str, converter_t converter))(va_list)
+length_modifier_t *my_get_length_modifier(char *str, converter_t *converter)
 {
-    char *(*convertion)(va_list) = NULL;
-
-    if (!my_is_int_converter(converter.symbol))
+    if (!my_is_int_converter(converter->symbol))
         return (NULL);
     str = my_revstr(str);
-    if (str[0] == 'h') {
-        convertion = converter.convertion;
-    } else if (str[0] == 'l') {
-        if (my_is_int_signed_converter(converter.symbol))
-            convertion = &my_llint_to_strnum;
-        else if (my_is_int_unsigned_converter(converter.symbol))
-            return (get_ll_convertion(converter.symbol));
-    }
-    return (convertion);
+    if (str[0] == 'h')
+        return (get_h_or_hh_length_modifier(str, converter));
+    else if (str[0] == 'l')
+        return (get_l_or_ll_length_modifier(str, converter));
+    return (NULL);
 }
 
-static char *(*get_ll_convertion(char symbol))(va_list)
+static length_modifier_t *get_h_or_hh_length_modifier(char *rev_str,
+                                                    converter_t *converter)
+{
+    length_modifier_t *length_modifier = malloc(sizeof(length_modifier));
+
+    if (rev_str[1] == 'h')
+        length_modifier->symbols = "hh";
+    else
+        length_modifier->symbols = "h";
+    length_modifier->convertion = converter->convertion;
+    return (length_modifier);
+}
+
+static length_modifier_t *get_l_or_ll_length_modifier(char *rev_str,
+                                                    converter_t *converter)
+{
+    length_modifier_t *length_modifier = malloc(sizeof(length_modifier));
+
+    if (rev_str[1] == 'l')
+        length_modifier->symbols = "ll";
+    else
+        length_modifier->symbols = "l";
+    if (my_is_int_signed_converter(converter->symbol)) {
+        length_modifier->convertion = &my_llint_to_strnum;
+        return (length_modifier);
+    }
+    length_modifier->convertion = get_unsigned_convertion(converter->symbol);
+    return (length_modifier);
+}
+
+static char *(*get_unsigned_convertion(char symbol))(va_list)
 {
     char *(*convertion)(va_list) = NULL;
 

1a9af5ada0fb18c27ff262f84cfc88222309cd17
Julien Pause <julien.pause@epitech.eu>
2019-11-17 15:16:17 +0400

    Rename test_my_printf_no_args to test_my_printf_special_cases

---
+++ tests/test_my_printf_special_cases.c
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2019
+** my_printf
+** File description:
+** Unit tests for my_printf with no variadic arguments
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "my.h"
+
+Test(my_printf_special_cases, no_args_hello_world, .init = cr_redirect_stdout)
+{
+    my_printf("hello world");
+    cr_assert_stdout_eq_str("hello world");
+}
+
+Test(my_printf_special_cases, no_args_one_char, .init = cr_redirect_stdout)
+{
+    my_printf("h");
+    cr_assert_stdout_eq_str("h");
+}
+
+Test(my_printf_special_cases, no_args_one_newline, .init = cr_redirect_stdout)
+{
+    my_printf("\n");
+    cr_assert_stdout_eq_str("\n");
+}
+
+Test(my_printf_special_cases, format_not_found, .init = cr_redirect_stdout)
+{
+    my_printf("hey this is my number : %-014.16hh");
+    cr_assert_stdout_eq_str("hey this is my number : %-014.16hh");
+}
+
+Test(my_printf_special_cases, unknown_flag, .init = cr_redirect_stdout)
+{
+    int nb = 42;
+
+    my_printf("hey this is my number : %- v+14.16hhd", nb);
+    cr_assert_stdout_eq_str("hey this is my number : %- v+14.16hhd");
+}
+
+Test(my_printf_special_cases, incorrect_precision, .init = cr_redirect_stdout)
+{
+    int nb = 42;
+
+    my_printf("hey this is my number : %.vd", nb);
+    cr_assert_stdout_eq_str("hey this is my number : %.vd");
+}
+
+Test(my_printf_special_cases, percentage_flag, .init = cr_redirect_stdout)
+{
+    my_printf("hey this is my percentage : %+- 18.16hh%");
+    cr_assert_stdout_eq_str("hey this is my percentage : %");
+}
